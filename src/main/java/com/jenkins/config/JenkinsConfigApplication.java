afe77bac8e9574a863428787f47d85b77b32db30
Vipin-km <vipinkm30@gmail.com>
2022-06-25 21:38:18 +0530

    added log

--- src/main/java/com/jenkins/config/JenkinsConfigApplication.java
+++ src/main/java/com/jenkins/config/JenkinsConfigApplication.java
@@ -1,13 +1,21 @@
 package com.jenkins.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
+@Slf4j
 public class JenkinsConfigApplication {
 
+	@PostConstruct
+	public void init(){
+		log.info("Application has been started --------");
+	}
 	public static void main(String[] args) {
+		log.info("Application executed --------");
 		SpringApplication.run(JenkinsConfigApplication.class, args);
 	}
-
 }
--- src/test/java/com/jenkins/config/JenkinsConfigApplicationTests.java
+++ src/test/java/com/jenkins/config/JenkinsConfigApplicationTests.java
@@ -1,13 +1,18 @@
 package com.jenkins.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
+@Slf4j
 class JenkinsConfigApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
-
+			log.info("Execuring the test --------");
+			assertEquals(true, true);
+		}
 }
