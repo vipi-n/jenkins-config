7e92a09c383a0790cd423775842c1fe031101a6d
Vipin-km <vipinkm30@gmail.com>
2022-06-25 21:11:35 +0530

    first commit

---
+++ src/test/java/com/jenkins/config/JenkinsConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
